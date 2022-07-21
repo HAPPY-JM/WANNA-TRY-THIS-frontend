5f1d9b23f63c4b7ba272529fd8a7486bfb52b894
GitSeongGit <102665966+GitSeongGit@users.noreply.github.com>
2022-07-21 17:56:06 +0900

    🎨 글로버 파일추가

---
+++ client/src/GlobalSyyle.jsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+
+const GlobalStyle = createGlobalStyle`
+     html,
+      body {
+        width: 100%;
+        height: 100%;
+        background-color: black;
+      }
+
+`
+
+
+export default GlobalStyle;
