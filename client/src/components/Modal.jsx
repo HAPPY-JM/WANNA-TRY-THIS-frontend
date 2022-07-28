e0cb5013c2ecd03f1e24d5d5382f9b7cbaa38d6e
lcy <cymarch98@gmail.com>
2022-07-29 02:18:35 +0900

    🎨 로그인 창이 새 창으로 뜨는 현상 수정

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -9,36 +9,39 @@ const Modal = () => {
   return (
     <>
       <div>
-              <button onClick={openModalHandler}>{!isOpen ? 'close' : 'open'}</button>
-              {!isOpen ? (
-                  <div> <a href="http://localhost:5000/api/auth/kakao" target="_blank" rel='noreferrer'>
-        <img
-          className="kakaoLogo"
-          src="../login/login-kakao.png"
-          alt="카카오 로그인"
-          width="200vh"
-          value="kakao"
-          target="_blank"
-        />
-      </a>
-      <a href="http://localhost:5000/api/auth/naver" target="_blank" rel='noreferrer'>
-        <img
-          className="naverLogo"
-          src="../login/login-naver.png"
-          alt="네이버 로그인"
-          width="200vh"
-          value="naver"
-        />
-      </a>
-      <a href="http://localhost:5000/api/auth/google" target="_blank" rel='noreferrer' >
-        <img
-          className="googleLogo"
-          src="../login/login-google.png"
-          alt="구글 로그인"
-          width="200vh"         
-        />
-      </a></div>
-              ):null}
+        <button onClick={openModalHandler}>{!isOpen ? 'close' : 'open'}</button>
+        {!isOpen ? (
+          <div>
+            {' '}
+            <a href="http://localhost:5000/api/auth/kakao" rel="noreferrer">
+              <img
+                className="kakaoLogo"
+                src="../login/login-kakao.png"
+                alt="카카오 로그인"
+                width="200vh"
+                value="kakao"
+              />
+            </a>
+            <a href="http://localhost:5000/api/auth/naver" rel="noreferrer">
+              <img
+                className="naverLogo"
+                src="../login/login-naver.png"
+                alt="네이버 로그인"
+                width="200vh"
+                value="naver"
+              />
+            </a>
+            <a href="http://localhost:5000/api/auth/google" rel="noreferrer">
+              <img
+                className="googleLogo"
+                src="../login/login-google.png"
+                alt="구글 로그인"
+                width="200vh"
+                value="google"
+              />
+            </a>
+          </div>
+        ) : null}
       </div>
     </>
   );
