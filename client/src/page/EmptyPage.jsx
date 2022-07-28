bd577d8ab699395f22a30ea5e28e42014421d56d
lcy <cymarch98@gmail.com>
2022-07-29 02:58:30 +0900

    ✨ emptypage로 들어오면 로그인 전 진행중이던 페이지로 이동

--- client/src/page/EmptyPage.jsx
+++ client/src/page/EmptyPage.jsx
@@ -1,4 +1,11 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 const EmptyPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => navigate(-2), []);
+
   return;
 };
 
